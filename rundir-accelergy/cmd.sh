0fea891e52c1237f40e1d93d56f78586e02be1b7
Qantals <aheng_ah@126.com>
2026-07-03 13:35:32 +0800

    update cmd.sh to use a specific output directory for yolo_tiny_test

--- rundir-accelergy/cmd.sh
+++ rundir-accelergy/cmd.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
-mkdir -p output
-./run_all.sh -c ../configs/scale.cfg -t ../topologies/conv_nets/test.csv -p ../test_runs/ -o ./output > ./output/run.log 2>&1
+OUTPUT_DIR=output_yolo_tiny_test
+mkdir -p "$OUTPUT_DIR"
+./run_all.sh -c ../configs/scale.cfg -t ../topologies/conv_nets/yolo_tiny.csv -p ../test_runs/ -o "$OUTPUT_DIR" > "${OUTPUT_DIR}/run.log" 2>&1
